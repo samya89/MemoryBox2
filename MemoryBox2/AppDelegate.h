93a1d1f840bb6e5e3062cebd0b4262a3498d4360
Samya <samya89@gmail.com>
2015-04-22 10:00:31 -0700

    Initial Commit

---
+++ MemoryBox2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MemoryBox2
+//
+//  Created by Samia Al Rahmani on 4/22/15.
+//  Copyright (c) 2015 Samya Al Rahmani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MemoryBox2Tests/MemoryBox2Tests.m
@@ -0,0 +1,40 @@
+//
+//  MemoryBox2Tests.m
+//  MemoryBox2Tests
+//
+//  Created by Samia Al Rahmani on 4/22/15.
+//  Copyright (c) 2015 Samya Al Rahmani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface MemoryBox2Tests : XCTestCase
+
+@end
+
+@implementation MemoryBox2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
